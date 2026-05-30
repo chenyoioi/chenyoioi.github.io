ebf44fa67409ccf4d00136416ddeafd46fa76e5f
chenyoioi <1535743858@qq.com>
2026-05-30 17:15:25 +0800

    Site updated: 2026-05-30 17:15:19

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["202605306856/","2026053027139/","202602231529496837/","202602081855693957/"];function toRandomPost(){
+var posts=["202602231529496837/","202602081855693957/","202605306856/","2026053027139/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
