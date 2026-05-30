9f8a553d258cfe22a752051967a2fc14b1ea4651
chenyoioi <1535743858@qq.com>
2026-05-30 14:53:23 +0800

    Site updated: 2026-05-30 14:53:19

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/02/08/旅游随记/","2026/02/23/摸鱼/"];function toRandomPost(){
+var posts=["2026/02/23/摸鱼/","2026/02/08/旅游随记/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
