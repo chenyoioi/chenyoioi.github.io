4a51aec6f20f3a4ba828b406cd8ee8c2676cc3f5
chenyoioi <1535743858@qq.com>
2026-05-30 16:55:34 +0800

    Site updated: 2026-05-30 16:55:28

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["202602231529496837/","202605306856/","202602081855693957/"];function toRandomPost(){
+var posts=["202602231529496837/","2026053027139/","202602081855693957/","202605306856/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
