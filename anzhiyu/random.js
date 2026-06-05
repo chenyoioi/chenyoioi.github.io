45833077092bd74373363817a49233fc2d330e96
chenyoioi <1535743858@qq.com>
2026-06-05 15:53:24 +0800

    Site updated: 2026-06-05 15:53:24

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["202605306856/","2026060550308/","202602231529496837/","2026053027139/","202602081855693957/"];function toRandomPost(){
+var posts=["2026060550308/","202602231529496837/","202602081855693957/","2026053027139/","202605306856/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
