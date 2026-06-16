50ac40d92339419fe83adcc02d28cd3c16b16398
chenyoioi <1535743858@qq.com>
2026-06-16 17:43:22 +0800

    Site updated: 2026-06-16 17:43:22

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/平生无策/","posts/摸鱼/","posts/旅游随记/","posts/标签用法手册/","posts/系统激活工具/","posts/某教育平台视频下载完整指南/"];function toRandomPost(){
+var posts=["posts/摸鱼/","posts/平生无策/","posts/旅游随记/","posts/某教育平台视频下载完整指南/","posts/标签用法手册/","posts/系统激活工具/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
