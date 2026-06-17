4ccd993fc6eb5e2a5b4894afe542c5bc54f48d93
chenyoioi <1535743858@qq.com>
2026-06-17 19:28:15 +0800

    Site updated: 2026-06-17 19:28:15

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/平生无策/","posts/Agnes免费Api使用手册/","posts/摸鱼/","posts/旅游随记/","posts/某教育平台视频下载完整指南/","posts/标签用法手册/","posts/系统激活工具/"];function toRandomPost(){
+var posts=["posts/平生无策/","posts/Agnes免费Api使用手册/","posts/旅游随记/","posts/摸鱼/","posts/某教育平台视频下载完整指南/","posts/系统激活工具/","posts/标签用法手册/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
